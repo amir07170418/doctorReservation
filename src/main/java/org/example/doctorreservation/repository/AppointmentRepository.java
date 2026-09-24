61b08daa2bf1b47e302f4dc2b7d9be96f0f9e6ba
amir07170418 <hossein13810418@gmail.com>
2026-09-24 23:52:09 +0330

    repository updated

--- src/main/java/org/example/doctorreservation/repository/AppointmentRepository.java
+++ src/main/java/org/example/doctorreservation/repository/AppointmentRepository.java
@@ -1,9 +1,20 @@
 package org.example.doctorreservation.repository;
 
 import org.example.doctorreservation.model.Appointment;
+import org.example.doctorreservation.model.AppointmentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    @Query("select count(a)>0 from Appointment a where a.doctor.id=:doctorId and " +
+            "a.appointmentDate=:appointmentDate and a.startTime<:endtime and a.endTime>:startTime and" +
+            " a.status!=org.example.doctorreservation.model.AppointmentStatus.CANCELLED")
+    boolean existsByTime(@Param("doctorId")Long doctorId, @Param("appointmentDate")LocalDate date, @Param("startTime")
+    LocalTime startTime, @Param("endTime")LocalTime endTime);
 }

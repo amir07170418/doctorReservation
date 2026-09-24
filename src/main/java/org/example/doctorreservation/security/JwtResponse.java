b83692f8007869db47078275e67c70d40c44bbc4
amir07170418 <hossein13810418@gmail.com>
2026-09-24 23:54:13 +0330

    jwt added

---
+++ src/main/java/org/example/doctorreservation/security/JwtResponse.java
@@ -0,0 +1,38 @@
+package org.example.doctorreservation.security;
+
+public class JwtResponse {
+    private String token;
+    private String username;
+
+    public JwtResponse(String token, String username) {
+        this.token = token;
+        this.username = username;
+    }
+
+    public JwtResponse() {
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "JwtResponse{" +
+                "token='" + token + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
